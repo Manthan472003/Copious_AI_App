03267921becf904a3878689484c98a83a6780882
Manthan472003 <manthandeodhe@gmail.com>
2024-10-25 17:01:18 +0530

    secont commithird commitat

--- AI_App/babel.config.js
+++ AI_App/babel.config.js
@@ -1,3 +1,10 @@
 module.exports = {
-  presets: ['module:metro-react-native-babel-preset'],
+  presets: [
+    ["module:metro-react-native-babel-preset", { loose: true }]
+  ],
+  plugins: [
+    ["@babel/plugin-transform-private-methods", { loose: true }],
+    ["@babel/plugin-transform-class-properties", { loose: true }],
+    ["@babel/plugin-transform-private-property-in-object", { loose: true }]
+  ]
 };
